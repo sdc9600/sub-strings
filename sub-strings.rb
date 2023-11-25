1b6075c27df614b48e7086043dae655b4d9c2322
sdc9600 <149299980+sdc9600@users.noreply.github.com>
2023-11-25 10:52:51 +1030

    Program now splits string into words

--- sub-strings.rb
+++ sub-strings.rb
@@ -2,17 +2,26 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 def substrings(string, dictionary)
   hash = {}
+  word_array = string.split(" ")
   dictionary.each {|value| hash[value] = 0}
-  dictionary.each do 
-    |word|
-    if string.include?(word)
-      hash[word] += 1
+  dictionary.each do
+    |dictionary_word|
+    word_array.each do
+      |string_word| 
+    if string_word.include?(dictionary_word)
+      hash[dictionary_word] += 1
     end
   end
+  end
+  hash = hash.select {|key, value| value >= 1}
+  if hash.empty?
+    puts "No sub-strings of the given string found in the passed dictionary!"
+  else
   puts hash
+  end
 end
 
-puts "Enter a string to find out how many sub-string matches you have with select words."
+puts "Enter a string to find out how many sub-string matches you have with a passed dictionary."
 string = gets.chomp
 
 substrings(string, dictionary)
